5d3c18af1473778d7da1de0735d9f80e3b043f82
Julius <julius.kurvers@gmail.com>
2026-04-28 21:30:44 +0200

    input validation

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use catMaybes" #-}
 
 module Main where
 
@@ -11,6 +13,8 @@ import Network.HTTP.Types
 import Text.Blaze.Html
 import qualified Text.Blaze.Html5 as H
 import GHC.Generics (Generic)
+import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Text.Encoding
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString as BS
@@ -21,66 +25,74 @@ import Text.Blaze.Html.Renderer.Utf8
 
 import KBG.Auth
 
+-- | (field id/name, label text, placeholder)
+formFields :: [(Text, Text, Text)]
+formFields =
+    [ ("ond",   "Onderwijs",     "Jari van Polen")
+    , ("int",   "Intern",        "Rens van Moorsel")
+    , ("sec",   "Secretaris",    "Bram de Haas")
+    , ("voo",   "Voorzitter",    "Iris van der Zwart")
+    , ("pen",   "Penningmeester","Chion Craane")
+    , ("ext",   "Extern",        "Isabelle Wittebols")
+    ]
+
 layout :: Markup -> Markup
 layout body = H.html $ do
     H.head $ do
-        H.title (text "My website!")
-    H.body body -- add nav bar
+        H.title "KandidaatsBestuurGokken"
+        H.style $ H.toHtml ("input.error { border: 2px solid red; } \
+                             \.error-msg { color: red; font-size: 0.85em; }" :: Text)
+    H.body body
 
 page :: Markup
 page = layout $ do
-    H.form ! HA.action "/" ! HA.method "post" $ do
-        H.label ! HA.for "onderwijs" $ "Onderwijs: "
-        H.input ! HA.type_ "text"
-                ! HA.id "onderwijs"
-                ! HA.name "onderwijs"
-                ! HA.placeholder "Jari van Polen"
-        H.br
-
-        H.label ! HA.for "intern" $ "Intern: "
-        H.input ! HA.type_ "text"
-                ! HA.id "intern"
-                ! HA.name "intern"
-                ! HA.placeholder "Rens van Moorsel"
-        H.br
-
-        H.label ! HA.for "secretaris" $ "Secretaris: "
-        H.input ! HA.type_ "text"
-                ! HA.id "secretaris"
-                ! HA.name "secretaris"
-                ! HA.placeholder "Bram de Haas"
-        H.br
-
-        H.label ! HA.for "voorzitter" $ "Voorzitter: "
-        H.input ! HA.type_ "text"
-                ! HA.id "voorzitter"
-                ! HA.name "voorzitter"
-                ! HA.placeholder "Iris van der Zwart"
-        H.br
-
-        H.label ! HA.for "penningmeester" $ "Penningmeester: "
-        H.input ! HA.type_ "text"
-                ! HA.id "penningmeester"
-                ! HA.name "penningmeester"
-                ! HA.placeholder "Chion Craane"
-        H.br
-
-        H.label ! HA.for "extern" $ "Extern: "
-        H.input ! HA.type_ "text"
-                ! HA.id "extern"
-                ! HA.name "extern"
-                ! HA.placeholder "Isabelle Wittebols"
-        H.br
-
+    H.form ! HA.action "/" ! HA.method "post" ! HA.id "mainForm" $ do
+        mapM_ renderField formFields
         H.input ! HA.type_ "submit" ! HA.value "Submit"
+    validationScript
+
+renderField :: (Text, Text, Text) -> Markup
+renderField (fieldId, label, placeholder) = do
+    H.label ! HA.for (textValue fieldId) $ H.toHtml (label <> ": ")
+    H.input ! HA.type_ "text"
+            ! HA.id    (textValue fieldId)
+            ! HA.name  (textValue fieldId)
+            ! HA.placeholder (textValue placeholder)
+    H.span  ! HA.id (textValue (fieldId <> "-error"))
+            ! HA.class_ "error-msg" $ ""
+    H.br
+
+validationScript :: Markup
+validationScript = H.script $ H.toHtml $ T.unlines
+    [ "document.getElementById('mainForm').addEventListener('submit', function(e) {"
+    , "  var fields = " <> fieldList <> ";"
+    , "  var ok = true;"
+    , "  fields.forEach(function(id) {"
+    , "    var el = document.getElementById(id);"
+    , "    var err = document.getElementById(id + '-error');"
+    , "    if (!el.value.trim()) {"
+    , "      el.classList.add('error');"
+    , "      err.textContent = ' This field is required.';"
+    , "      ok = false;"
+    , "    }"
+    , "    el.addEventListener('input', function() {"
+    , "      el.classList.remove('error');"
+    , "      err.textContent = '';"
+    , "    }, { once: true });"
+    , "  });"
+    , "  if (!ok) e.preventDefault();"
+    , "});"
+    ]
+  where
+    fieldList = "['" <> T.intercalate "','" (map (\(f,_,_) -> f) formFields) <> "']"
 
 data Bettee = Bettee
-    { name :: String
+    { name     :: String
     , function :: String
     } deriving (Show, Generic, FromJSON)
 
 data Bet = Bet
-    { better :: String
+    { better  :: String
     , bettees :: [Bettee]
     } deriving (Show, Generic, FromJSON)
 
@@ -91,35 +103,28 @@ main = do
     let settings = setPort port_ $ setHost host_ defaultSettings
     putStrLn $ "listening on " ++ show host_ ++ ":" ++ show port_
     contents <- (decode <$> BSL.readFile "./data.json") :: IO (Maybe [Bet])
-    -- print contents
+    print contents
     manager <- newTlsManager
     disc <- discovery manager
     runSettings settings (requireSession manager disc app)
 
 app :: Application
-app req res = do
-    case requestMethod req of
-        "GET" -> res $ responseLBS status200 [("Content-Type", "text/html")] (renderHtml page)
-        "POST" -> do
-            (params, _files) <- parseRequestBody lbsBackEnd req
-            let onderwijs = lookupParam "onderwijs" params
-                intern = lookupParam "intern" params
-                secretaris = lookupParam "secretaris" params
-                voorzitter = lookupParam "voorzitter" params
-                penningmeester = lookupParam "penningmeester" params
-                extern = lookupParam "extern" params
-                inp = [onderwijs, intern, secretaris, voorzitter, penningmeester, extern]
-            let response = (if Prelude.any (\s -> s == Just "\"\"") inp then "No input provided" else "Found, " <> show inp)
-            res $ responseLBS status200 [("Content-Type", "text/html")] (renderHtml $ H.p $ H.toHtml response)
-        _ -> res $ responseLBS status405 [("Content-Type", "text/plain")] "Method not allowed"
+app req res = case requestMethod req of
+    "GET"  -> res $ htmlResponse status200 (renderHtml page)
+    "POST" -> do
+        (params, _) <- parseRequestBody lbsBackEnd req
+        let values = map (\(f,_,_) -> lookupParam f params) formFields
+        let response = T.intercalate ", " [v | Just v <- values]
+        res $ htmlResponse status200 (renderHtml . H.p $ H.toHtml response)
+    _ -> res $ responseLBS status405 [("Content-Type", "text/plain")] "Method not allowed"
 
+htmlResponse :: Status -> BSL.ByteString -> W.Response
+htmlResponse status = responseLBS status [("Content-Type", "text/html")]
 
-lookupParam :: BS.ByteString -> [(BS.ByteString, BS.ByteString)] -> Maybe String
-lookupParam key params = fmap (show . decodeUtf8) (lookup key params)
+lookupParam :: Text -> [(BS.ByteString, BS.ByteString)] -> Maybe Text
+lookupParam key params = decodeUtf8 <$> lookup (encodeUtf8 key) params
+  where encodeUtf8 = Data.Text.Encoding.encodeUtf8
 
 notFoundRoute :: W.Response
-notFoundRoute =
-    responseLBS
-    status404
-    [("Content-Type", "text/html")]
-    "404 - Not Found <br>Have you tried turning it off and on again?"
+notFoundRoute = responseLBS status404 [("Content-Type", "text/html")]
+    "404 - Not Found<br>Have you tried turning it off and on again?"
